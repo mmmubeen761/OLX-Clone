29de5a6358ac3184860397d4b2bf26592bf12247
ubeenMalikgit config --global user.name Mubeen <mmmubee761@gmail.com>
2020-11-13 02:06:58 +0500

    Olx Clone App

---
+++ precache-manifest.b9ee5b2d23ec4091fc258814c172fb4a.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da55823a14fbf7ec4b743299496f297a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54e7c512a2b9b2b0c90d",
+    "url": "/static/css/2.84c1e8e6.chunk.css"
+  },
+  {
+    "revision": "bb6b648d1a138fefde6b",
+    "url": "/static/css/main.26c6663a.chunk.css"
+  },
+  {
+    "revision": "54e7c512a2b9b2b0c90d",
+    "url": "/static/js/2.e581f533.chunk.js"
+  },
+  {
+    "revision": "cbb71c20b7296e6bd7ab3fcce0ba44d8",
+    "url": "/static/js/2.e581f533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6b648d1a138fefde6b",
+    "url": "/static/js/main.39c08bf6.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "e25b4b6a1118dc24ae141069962665c9",
+    "url": "/static/media/5000.e25b4b6a.jpg"
+  },
+  {
+    "revision": "a48f720b061f4a2c1e42dabe938eb236",
+    "url": "/static/media/Cam.a48f720b.jpeg"
+  },
+  {
+    "revision": "eb75410e0abefbbb102bf0f35d0ccbf0",
+    "url": "/static/media/Cock.eb75410e.jpeg"
+  },
+  {
+    "revision": "cbb7e9098db41f3aca232f347cd3be74",
+    "url": "/static/media/OT.cbb7e909.jpeg"
+  },
+  {
+    "revision": "638181100538123713772ceb95a45777",
+    "url": "/static/media/hero_bg_pk.63818110.jpg"
+  },
+  {
+    "revision": "e86721a89fc44bda4d961fd9cd3c8839",
+    "url": "/static/media/img1.e86721a8.png"
+  },
+  {
+    "revision": "31a53f730c900235a8a4552928209d35",
+    "url": "/static/media/img2.31a53f73.png"
+  },
+  {
+    "revision": "05941531f3f70a8611124d902c6935fc",
+    "url": "/static/media/img3.05941531.png"
+  },
+  {
+    "revision": "3be35ec4ef1356288e33cd8fd89e5033",
+    "url": "/static/media/img5.3be35ec4.png"
+  },
+  {
+    "revision": "e49489656fa0467cc81e120c6be471ef",
+    "url": "/static/media/img6.e4948965.png"
+  },
+  {
+    "revision": "d7f67bc07a67fe587abac336f2c150b8",
+    "url": "/static/media/img7.d7f67bc0.JPG"
+  },
+  {
+    "revision": "a642472e2810a02c70e626cb813d8e70",
+    "url": "/static/media/map.a642472e.jpg"
+  },
+  {
+    "revision": "dc76cf41e0fe73fadddd3e138bcd3e90",
+    "url": "/static/media/notifications.dc76cf41.png"
+  },
+  {
+    "revision": "4cb8cbc478714518f696fe164d5f8157",
+    "url": "/static/media/phone-app.4cb8cbc4.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "0d0b907d90c48dc04dc4348f550f3c87",
+    "url": "/static/media/unnamed.0d0b907d.png"
+  },
+  {
+    "revision": "ee65102192a339b000051ba7437958bc",
+    "url": "/static/media/unnamed.ee651021.jpg"
+  }
+]);
